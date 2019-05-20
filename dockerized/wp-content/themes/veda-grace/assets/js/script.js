4d1a2dbb46a3be97cc03370224dd376bd38b2b8e
Ubuntu <ubuntu@ip-172-31-26-20.us-east-2.compute.internal>
2019-05-20 20:17:11 +0000

    removed dockerized directory and changed the docker-compose.yml

--- dockerized/wp-content/themes/veda-grace/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.mobile-button').click(function(e){
-        e.preventDefault();
-        $(this).toggleClass('opened');
-        $('.mobile-menu').slideToggle(200);
-    });
-});
